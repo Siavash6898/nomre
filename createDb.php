5709ced5e9bec9c6a6257794b0d4377d63f71f0f
Siavash6898 <137829269+Siavash6898@users.noreply.github.com>
2024-12-25 10:25:28 +0100

    Update createDb.php

--- createDb.php
+++ createDb.php
@@ -1,7 +1,7 @@
 <?php
 // اطلاعات اتصال به MySQL test7
 $host = 'localhost'; // نام host
-$username = 'roooot'; // نام کاربری MySQL
+$username = 'root'; // نام کاربری MySQL
 $password = ''; // رمز عبور MySQL
 $dbname = 'my_new_database'; // نام دیتابیس مورد نظر
 
