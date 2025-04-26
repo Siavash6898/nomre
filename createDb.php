9867c785f78d914c643ff968f941f40761798ec4
Siavash6898 <137829269+Siavash6898@users.noreply.github.com>
2025-04-26 09:58:09 +0200

    Update createDb.php

--- createDb.php
+++ createDb.php
@@ -1,5 +1,5 @@
 <?php
-// اطلاعات اتصال به MySQL test3
+// اطلاعات اتصال به MySQL test4
 $host = 'localhost'; // نام host
 $username = 'root'; // نام کاربری MySQL
 $password = 'vad'; // رمز عبور MySQL
